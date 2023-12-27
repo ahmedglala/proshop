f522e948d49743b4846fd7e87677bb5f79463ada
ahmedglala <glala2954@gmail.com>
2023-12-28 01:27:52 +0200

    not important

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <p>
-      welcome to proshop
-    </p>
+    <h1 className="text-red-500 text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
